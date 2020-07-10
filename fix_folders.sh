3759a99600bc4b4496e5a30660d55bb954894c80
Mickroz <Mickroz@users.noreply.github.com>
2020-07-10 19:16:33 +0200

    changes
    changes

--- fix_folders.sh
+++ fix_folders.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 # script to remove wd_tv live stuff.
-find /volume1/Movies/. -name '.wd_tv' -delete
-find /volume1/Movies/. -name 'flixster_lookup_cache*' -delete
-find /volume1/Movies/. -name 'backdrop.jpg' -delete
-find /volume1/Movies/. -name '.*.backdrop' -delete
-find /volume1/Movies/. -name '*.nfo' -delete
-find /volume1/Movies/. -name '*.txt' -delete
-find /volume1/Movies/. -name 'Thumbs.db' -delete
-find /volume1/Series/. -name '.wd_tv' -delete
-find /volume1/Series/. -name 'flixster_lookup_cache*' -delete
-find /volume1/Series/. -name 'backdrop.jpg' -delete
-find /volume1/Series/. -name '.*.backdrop' -delete
-find /volume1/Series/. -name '*.nfo' -delete
-find /volume1/Series/. -name '*.txt' -delete
-find /volume1/Series/. -name 'Thumbs.db' -delete
 find /volume1/. -type d -name '@eaDir' -print0 | xargs -0 rm -rf
+find /volume1/films/. -name '.wd_tv' -delete
+find /volume1/films/. -name 'flixster_lookup_cache*' -delete
+find /volume1/films/. -name 'backdrop.jpg' -delete
+find /volume1/series/. -name '*.banner.jpg' -delete
+find /volume1/series/. -name '*.background.jpg' -delete
+find /volume1/films/. -name '.*.backdrop' -delete
+find /volume1/films/. -name '*.nfo' -delete
+find /volume1/films/. -name '*.txt' -delete
+find /volume1/films/. -name 'Thumbs.db' -delete
+find /volume1/films/. -name '*.jpg' -delete
+find /volume1/films/. -name '*.xml' -delete
+find /volume1/films/. -name '*.bif' -delete
+find /volume1/films/. -name '*.idx' -delete
+find /volume1/films/. -type f -name '*.srt.random*' -exec bash -c 'mv "$1" "${1%.srt.random*}".nl.srt' - '{}' \;
+find /volume1/series/. -name '.wd_tv' -delete
+find /volume1/series/. -name 'flixster_lookup_cache*' -delete
+find /volume1/series/. -name 'backdrop.jpg' -delete
+find /volume1/films/. -name '*.banner.jpg' -delete
+find /volume1/films/. -name '*.background.jpg' -delete
+find /volume1/series/. -name '.*.backdrop' -delete
+find /volume1/series/. -name '*.nfo' -delete
+find /volume1/series/. -name '*.txt' -delete
+find /volume1/series/. -name 'Thumbs.db' -delete
