6f9bdfb7c8481ad1dcf0039e2c55731c48a21c2b
Mickroz <Mickroz@users.noreply.github.com>
2020-07-10 19:17:17 +0200

    Update fix_folders.sh
    tidy

--- fix_folders.sh
+++ fix_folders.sh
@@ -4,8 +4,8 @@ find /volume1/. -type d -name '@eaDir' -print0 | xargs -0 rm -rf
 find /volume1/films/. -name '.wd_tv' -delete
 find /volume1/films/. -name 'flixster_lookup_cache*' -delete
 find /volume1/films/. -name 'backdrop.jpg' -delete
-find /volume1/series/. -name '*.banner.jpg' -delete
-find /volume1/series/. -name '*.background.jpg' -delete
+find /volume1/films/. -name '*.banner.jpg' -delete
+find /volume1/films/. -name '*.background.jpg' -delete
 find /volume1/films/. -name '.*.backdrop' -delete
 find /volume1/films/. -name '*.nfo' -delete
 find /volume1/films/. -name '*.txt' -delete
@@ -18,8 +18,8 @@ find /volume1/films/. -type f -name '*.srt.random*' -exec bash -c 'mv "$1" "${1%
 find /volume1/series/. -name '.wd_tv' -delete
 find /volume1/series/. -name 'flixster_lookup_cache*' -delete
 find /volume1/series/. -name 'backdrop.jpg' -delete
-find /volume1/films/. -name '*.banner.jpg' -delete
-find /volume1/films/. -name '*.background.jpg' -delete
+find /volume1/series/. -name '*.banner.jpg' -delete
+find /volume1/series/. -name '*.background.jpg' -delete
 find /volume1/series/. -name '.*.backdrop' -delete
 find /volume1/series/. -name '*.nfo' -delete
 find /volume1/series/. -name '*.txt' -delete
